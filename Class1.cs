4ca388b56915d3ae1d2e6944d4e743c7448b422e
FebrianRidho <101778552+FebrianRidho@users.noreply.github.com>
2022-04-21 09:47:03 +0700

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4503
+{
+    public class Karyawan
+    {
+        public string NIK { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+
+        public Karyawan(string nik, string nama, int GajiBulan)
+        {
+            NIK = nik;
+            Nama = nama;
+            GajiBulanan = GajiBulan;
+            if(GajiBulanan < 0 )
+            {
+                GajiBulanan = 0;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4503
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan kar1 = new Karyawan("190302123", "Paijo", Int32.Parse("3000000"));
+            Karyawan kar2 = new Karyawan("190302124", "Jono", Int32.Parse("2000000"));
+
+            Console.WriteLine("No NIK / Nama \t Gaji Bulanan");
+            Console.WriteLine("-----------------------------");
+            CetakHasil11(kar1);
+            CetakHasil12(kar2);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Asyiiiiik dapat kenaikan gaji 10%!!");
+
+            Console.WriteLine();
+
+            Console.WriteLine("No NIK / Nama \t Gaji Bulanan");
+            Console.WriteLine("------------------------------");
+            CetakHasil13(kar1);
+            CetakHasil14(kar2);
+
+            Console.ReadKey();
+        }
+        static void CetakHasil11(Karyawan k)
+        {
+            Console.WriteLine("1. {0} {1}\t{2}", k.NIK, k.Nama, k.GajiBulanan);
+        }
+        static void CetakHasil12(Karyawan k)
+        {
+            Console.WriteLine("2. {0} {1}\t{2}", k.NIK, k.Nama, k.GajiBulanan);
+        }
+        static void CetakHasil13(Karyawan k)
+        {
+            Console.WriteLine("1. {0} {1}\t{2}", k.NIK, k.Nama, k.GajiBulanan + k.GajiBulanan * 10 / 100);
+        }
+        static void CetakHasil14(Karyawan k)
+        {
+            Console.WriteLine("2. {0} {1}\t{2}", k.NIK, k.Nama, k.GajiBulanan + k.GajiBulanan * 10 / 100);
+        }
+
+    }
+}
